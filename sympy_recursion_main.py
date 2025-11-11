8aa171b4e0c6d2d282635a5db37890dca671488f
Zachary Ziemba <zjohnzie@umich.edu>
2025-11-11 10:58:01 -0600

    Added Function Skeleton for IVP Calculations

--- sympy_recursion_main.py
+++ sympy_recursion_main.py
@@ -4,6 +4,31 @@ import numpy as np
 def euler_formula(theta):
     return sympy.cos(theta) + sympy.I*(sympy.sin(theta))
 
+def ivp_calculation(ivp_input):
+    if type(ivp_input) is list:
+        arr = np.array(ivp_input)
+        if arr.ndim > 2:
+            print("This is a 3-dimensional or higher tensor! This is not the correct data form!\n")
+            return
+        elif arr.ndim == 1:
+            print("this is not a matrix bruh\n")
+            return
+            
+        elif arr.shape[0] == 1 or arr.shape[1] == 1:
+            print("bruh this is a vector, not an effin matrix")
+            print("ill deal with this later as an assumed form of [f0, f1, ...] k?\n")
+            return
+        else:
+            print(":?\n")
+        matrix = sympy.Matrix(arr)
+        print(f"Original matrix is: {matrix}")
+        matrix = matrix.rref()[0]
+        print(f"Solved matrix is: {matrix}")
+        print('\n')
+
+        return matrix # TO BE CHANGED; NEED TO FIGURE OUT PROPER/BETTER FORM FOR IVP INPUTS AND OUTPUTS
+        
+
 
 def recursion_calculation(input_form):
     sol_eq = sympy.sympify(0)
@@ -78,5 +103,18 @@ recursion_calculation([1,-2,3,4,1])
 
 recursion_calculation([1,-2,3,4,1,1])
 
-
 recursion_calculation([1,0,0,0,1,2,4,0,5])
+
+ivp_calculation([1,1])
+
+ivp_calculation([[1],[2]])
+
+ivp_calculation([[1,0,1],[11,0,2],[1,1,1]])
+
+f0, f1, f2, f3, f4 = sympy.symbols('f0 f1 f2 f3 f4')
+
+sqrt5 = sympy.sqrt(5)
+
+ivp_calculation([[1,1,0,0,1,f0],[0,-sqrt5,1,1,1-(1+sqrt5)/2,f1],[0,-sqrt5,4,2,-(1+sqrt5)/2,f2],[0,-2*sqrt5,9,3,-2*(1+sqrt5)/2,f3],[0,-3*sqrt5,16,4,-1-3*(1+sqrt5)/2,f4]])
+
+
