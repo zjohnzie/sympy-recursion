89202bf9751930a5c55c3aa388a4e275c6ce31a6
Zachary Ziemba <zjohnzie@umich.edu>
2025-11-10 19:08:48 -0600

    Main functions written for list notation

---
+++ sympy_recursion_main.py
@@ -0,0 +1,82 @@
+import sympy
+import numpy as np
+
+def euler_formula(theta):
+    return sympy.cos(theta) + sympy.I*(sympy.sin(theta))
+
+
+def recursion_calculation(input_form):
+    sol_eq = sympy.sympify(0)
+    print(sol_eq)
+    if type(input_form) is list:
+        print(input_form)
+        print(str(len(input_form)) + " is the length of the list")
+        print(str(input_form.count(0)) + " is the number of zeroes")
+
+        if len(input_form) > 5:
+            print("Cannot find symbolic roots of any quintics (at least now)\n")
+        else:
+            print("Will do calculations!\n")
+            x, n = sympy.symbols('x n')
+            expr = sympy.sympify(0)
+            idx = 0
+
+            arr = np.array(input_form)
+            add_cnst = np.nonzero(arr)[0][-1]
+                
+            for coef in input_form:
+            #f_temp = sympy.symbols('f' + str(idx))
+                expr += coef*(x**(-idx+add_cnst))
+                idx += 1
+
+
+            imRootFlag = False
+            idx = 0
+            for root in sympy.roots(expr):
+                if imRootFlag:
+                    imRootFlag = False
+                    continue
+                if not root.is_real:
+                    imRootFlag = True
+                    c_temp = sympy.symbols('c' + str(idx))
+                    c_temp2 = sympy.symbols('c' + str(idx+1))
+                    a = sympy.re(root)
+                    b = sympy.im(root)
+                    sol_eq += c_temp*sympy.cos(n*sympy.acos(a/sympy.sqrt(a**2 + b**2))) + c_temp2*sympy.sin(n*sympy.Abs(sympy.asin(b/sympy.sqrt(a**2 + b**2))))
+                    idx += 2
+                else:
+                    c_temp = sympy.symbols('c' + str(idx))
+                    sol_eq += c_temp*(root**n)
+                    idx += 1
+                    
+
+            
+            print('\n')
+
+    print(sol_eq)
+    print('\n')
+    return sol_eq
+
+
+
+## Test Cases
+recursion_calculation([1,-2])
+
+recursion_calculation([1,0,1])
+
+recursion_calculation([1,5,2])
+
+recursion_calculation([1,-4,3])
+
+recursion_calculation([2,0,1,3,])
+
+recursion_calculation([0,1,1])
+
+recursion_calculation([1,0,2,2])
+
+recursion_calculation([1,-2,3,4,1])
+
+recursion_calculation([1,-2,3,4,1,1])
+
+
+recursion_calculation([1,0,0,0,1,2,4,0,5])
