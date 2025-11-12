080cf08f1cc86d15113e2cc877befa5db57705d3
Zachary Ziemba <zjohnzie@umich.edu>
2025-11-12 15:56:27 -0600

    Added more functional structure with main_function and format processing function; Now works on lists or numpy arrays for general solver

--- sympy_recursion_main.py
+++ sympy_recursion_main.py
@@ -4,6 +4,8 @@ import numpy as np
 def euler_formula(theta):
     return sympy.cos(theta) + sympy.I*(sympy.sin(theta))
 
+
+
 def ivp_calculation(ivp_input):
     if type(ivp_input) is list:
         
@@ -45,55 +47,52 @@ def ivp_calculation(ivp_input):
                 
 
         return constants
+
         
 
-
-def recursion_calculation(input_form):
+def recursion_solver(input_form):
     sol_eq = sympy.sympify(0)
     print(sol_eq)
-    if type(input_form) is list:
-        print(input_form)
-        print(str(len(input_form)) + " is the length of the list")
-        print(str(input_form.count(0)) + " is the number of zeroes")
+    listFlag = False
+    if isinstance(input_form, np.ndarray):
+        arr = input_form
+    elif isinstance(input_form, list): #idk how this would happen but including it anyways
+        listFlag = True
+        arr = np.array(input_form)
 
-        if len(input_form) > 5:
-            print("REC 1: Cannot find symbolic roots of any quintics (at least now)\n")
-        else:
-            print("REC 2: Will do calculations!\n")
-            x, n = sympy.symbols('x n')
-            expr = sympy.sympify(0)
-            idx = 0
-
-            arr = np.array(input_form)
-            add_cnst = np.nonzero(arr)[0][-1]
+    if arr.size > 5:
+        print("REC 1: Cannot find symbolic roots of any quintics (at least now)\n")
+    else:
+        print("REC 2: Will do calculations!\n")
+        x, n = sympy.symbols('x n')
+        expr = sympy.sympify(0)
+            
+        idx = 0
+        add_cnst = np.nonzero(arr)[0][-1]
                 
-            for coef in input_form:
-            #f_temp = sympy.symbols('f' + str(idx))
-                expr += coef*(x**(-idx+add_cnst))
+        for coef in input_form:
+            expr += coef*(x**(-idx+add_cnst)) #This needs an explanation imo
+            idx += 1
+
+        imRootFlag = False
+        idx = 0
+        for root in sympy.roots(expr):
+            if imRootFlag:
+                imRootFlag = False
+                continue
+            if not root.is_real:
+                imRootFlag = True
+                c_temp = sympy.symbols('c' + str(idx))
+                c_temp2 = sympy.symbols('c' + str(idx+1))
+                a = sympy.re(root)
+                b = sympy.im(root)
+                sol_eq += c_temp*sympy.cos(n*sympy.acos(a/sympy.sqrt(a**2 + b**2))) + c_temp2*sympy.sin(n*sympy.Abs(sympy.asin(b/sympy.sqrt(a**2 + b**2))))
+                idx += 2
+            else:
+                c_temp = sympy.symbols('c' + str(idx))
+                sol_eq += c_temp*(root**n)
                 idx += 1
 
-
-            imRootFlag = False
-            idx = 0
-            for root in sympy.roots(expr):
-                if imRootFlag:
-                    imRootFlag = False
-                    continue
-                if not root.is_real:
-                    imRootFlag = True
-                    c_temp = sympy.symbols('c' + str(idx))
-                    c_temp2 = sympy.symbols('c' + str(idx+1))
-                    a = sympy.re(root)
-                    b = sympy.im(root)
-                    sol_eq += c_temp*sympy.cos(n*sympy.acos(a/sympy.sqrt(a**2 + b**2))) + c_temp2*sympy.sin(n*sympy.Abs(sympy.asin(b/sympy.sqrt(a**2 + b**2))))
-                    idx += 2
-                else:
-                    c_temp = sympy.symbols('c' + str(idx))
-                    sol_eq += c_temp*(root**n)
-                    idx += 1
-                    
-
-            
             print('\n')
 
     print(sol_eq)
@@ -102,26 +101,120 @@ def recursion_calculation(input_form):
 
 
 
+def recursion_format_processing(input_form, *args, **kwargs):
+    
+    ### Vector format for recursion solving should be a row vector
+    ### with the i-th entry corresponding to the constant multiple of the f_{n-i} term of the recursive sequence equation 
+    ### The FINAL output will be a symbolic expression corresponding to the general real solution to the homogenous equation
+    ###
+    ### If a specific solution is desired over the general solution,
+    ### the vector format will require two row vectors where the second vector should have 
+    ### specified values corresponding to the value of f_{0}, f_{1}, and so on.
+    ###
+    ### String format should either be text corresponding to latex or sympy code
+    ###
+    ### Sympy format ignores variables not of the form of "f_{n-k}" or "f(n-k)" or "f_n-k"
+    ### where f can be any string of characters that are either first encountered
+    ### or are the 
+    ### Sympy format could also work independently of the choice of symbols for your variables
+    ### and just read left-to-right interpreting the i-th encountered variable as the f_{n-i} term
+    # Could implement sympy ignore_name flag in parameters for this
+    # Can work on seeing if a given set of initial conditions are sufficient to specify an entire solution
+    # as this seems plausible under certain structures of the sequence relation
+
+    print(type(input_form))
+    ivp_arr = []
+    if isinstance(input_form, list):
+        if len(input_form) == 2:
+            if isinstance(input_form[0], list):
+                ivp_arr = np.array(input_form[1])
+                input_form = np.array(input_form[0])
+                print(f"Your list is now proper with the sequence: {input_form} and the IVPs of: {ivp_arr}")
+                return input_form, ivp_arr, True
+
+        if all(isinstance(item, (int, float)) for item in input_form):
+            input_form = np.array(input_form)
+                
+
+    if isinstance(input_form, np.ndarray):
+        if input_form.ndim == 1:
+            print(f"This input has the following vector: {input_form}\n")
+            return input_form, np.array([]), True
+        elif input_form.ndim == 2:
+            # PROBABLY NEED TO ADD A CHECK FOR PROPER SHAPE OF THESE VECTORS!!!
+            print(f"The input has the following two vectors: {input_form[0]} and {input_form[1]}\n")
+            return input_form[0], input_form[1], True
+        elif input_form.ndim > 2:
+            print("PROC 1: Your input has too many input rows in your numpy array\n")
+            
+    elif isinstance(input_form, str):
+        print("PROC 2: Working on text inputs bud!!!\n")
+
+    elif isinstance(input_form, sympy.Add):
+        print("PROC 3: Working on sympy inputs PAL\n")
+        # need to test variety of sympy expression concepts as even 2*f0 is not consideredof the class "Add"
+        
+
+    return np.array([]),np.array([]), False 
+
+
+
+def sym_recursion_solver_main(sequence, *args, **kwargs):
+    print("We're doing this main baby!!!")
+    sequence_proper, ivp_proper, proper_flag = recursion_format_processing(sequence)
+    gen_solution = recursion_solver(sequence_proper)
+    if proper_flag:
+        if ivp_proper.size == 0:
+            if sequence_proper.size == 0:
+                print("MAIN 1: Your sequence is empty buddy!!")
+                return sequence_proper
+            else:
+                return gen_solution
+        else:
+            # NEEDS WORK DONE ASAP!!!
+            print("MAIN 3: CURRENTLY NOT WORKING OHMIGOSH IM SO SAWRRY")
+            return ivp_calculation(ivp_proper)
+    else:
+        print("MAIN 2: Your sequence is not an accepted format!")
+        
+    
+    
 ## Test Cases
-recursion_calculation([1,-2])
 
-recursion_calculation([1,0,1])
 
-recursion_calculation([1,5,2])
+print(f"{recursion_format_processing([1,-2])}\n")
 
-recursion_calculation([1,-4,3])
+print(f"{recursion_format_processing([1,0,1])}\n")
 
-recursion_calculation([2,0,1,3,])
+print(f"{recursion_format_processing([1,5,2])}\n")
 
-recursion_calculation([0,1,1])
+print(f"{recursion_format_processing([[1,2,3],[0,0,1]])}\n")
 
-recursion_calculation([1,0,2,2])
+str_test = "test"
+print(f"{recursion_format_processing(str_test)}\n")
 
-recursion_calculation([1,-2,3,4,1])
+sym_recursion_solver_main([1,-2])
 
-recursion_calculation([1,-2,3,4,1,1])
+print(len([[1,-2],[0]]))
+print(f"IVP solution is: {sym_recursion_solver_main([[1,-2],[0]])}")
 
-recursion_calculation([1,0,0,0,1,2,4,0,5])
+sym_recursion_solver_main([1,0,1])
+
+sym_recursion_solver_main([1,5,2])
+
+sym_recursion_solver_main([1,-4,3])
+
+sym_recursion_solver_main([2,0,1,3,])
+
+sym_recursion_solver_main([0,1,1])
+
+sym_recursion_solver_main([1,0,2,2])
+
+sym_recursion_solver_main([1,-2,3,4,1])
+
+sym_recursion_solver_main([1,-2,3,4,1,1])
+
+sym_recursion_solver_main([1,0,0,0,1,2,4,0,5])
 
 ivp_calculation([1,1])
 
