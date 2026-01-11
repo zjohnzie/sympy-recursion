ba93b605f04a574a59b2b529d81756127afe8085
Zachary Ziemba <zjohnzie@umich.edu>
2026-01-11 13:13:12 -0600

    Modified code to follow more standards of PEP 8; more work needed

--- sympy_recursion_main.py
+++ sympy_recursion_main.py
@@ -3,8 +3,10 @@ import sympy
 import numpy as np
 import re # may remove this...
 
+
 def euler_formula(theta):
     return sympy.cos(theta) + sympy.I*(sympy.sin(theta))
+
 
 def matrix_to_solution(matrix, *args, **kwargs):
 
@@ -14,12 +16,13 @@ def matrix_to_solution(matrix, *args, **kwargs):
         matrix = np.array(matrix)
     print(f"Matrix shape is: {matrix.shape}")
     print(f"Matrix dimension is: {matrix.ndim}")
-    if (matrix.ndim == 2 and (matrix.shape[0] == 1 or matrix.shape[1] == 1)) or matrix.ndim == 1:
+    if (matrix.ndim == 2 and (matrix.shape[0] == 1 or matrix.shape[1] == 1))
+        or matrix.ndim == 1:
             
         idx = 0
         for entry in matrix:
 
-            c_temp = sympy.symbols('c' + str(idx))
+            c_temp = sympy.symbols("c" + str(idx))
             if not isinstance(entry, (sympy.Add, sympy.Mul)):
                 if entry != None:
                     sol += c_temp * sympy.sympify(entry)
@@ -31,24 +34,24 @@ def matrix_to_solution(matrix, *args, **kwargs):
         return 
 
     return sol
-        
-        
+                
 
 def solution_to_matrix(gen_sol, *args, **kwargs):
 
-    ### Takes in a given general solution in the form of a sympy expression
-    ### And outputs a square matrix for solving a given set of initial values,
-    ### where the i,j-th entry corresponds to the j-th term of the given solution
-    ### without the generalized constant in front with the value of n substituted for i+1
-    ###
-    ### The rationale is that this matrix, A, satisfies:
-    ### \vec{f} = A\vec{b}
-    ### where f_{i} = c_{0}*term_{0}(i) + c_{1}*term_{1}(i) + ... + c_{n}*term_{n}(i)
-    ### and b_{i} = c_{i}
-
-    # Currently, this will only work on equations with less than 10 constants to be determined
-    # Will need to rewrite with a new recursive function to deal with indices with more than 1 digit
+    """
+     Takes in a given general solution in the form of a sympy expression
+     And outputs a square matrix for solving a given set of initial values,
+     where the i,j-th entry corresponds to the j-th term of the given solution
+     without the generalized constant in front with the value of n substituted for i+1
     
+     The rationale is that this matrix, A, satisfies:
+     \vec{f} = A\vec{b}
+     where f_{i} = c_{0}*term_{0}(i) + c_{1}*term_{1}(i) + ... + c_{n}*term_{n}(i)
+     and b_{i} = c_{i}
+
+     Currently, this will only work on equations with less than 10 constants to be determined
+     Will need to rewrite with a new recursive function to deal with indices with more than 1 digit
+    """
     matrix = [None] * len(sympy.Add.make_args(gen_sol))
     print(sympy.Add.make_args(gen_sol))
     for term in sympy.Add.make_args(gen_sol):
@@ -57,15 +60,17 @@ def solution_to_matrix(gen_sol, *args, **kwargs):
         for symbol in list(term.free_symbols):
             print(f"Current symbol is: {symbol}")
             idx_num = -1 # can this be moved outside of this loop?
-            if str(symbol)[0] == 'c':
+            if str(symbol)[0] == "c":
                 if str(symbol)[1].isdigit():
                     idx_num = int(str(symbol)[1])
-                elif str(symbol)[1] == '-' or str(symbol)[1] == '_':
+                elif str(symbol)[1] == "-" or str(symbol)[1] == "_":
                     if str(symbol)[2].isdigit():
                         idx_num = int(str(symbol)[2])
-                    elif str(symbol)[2] == '{':
+                    elif str(symbol)[2] == "{":
                         if str(symbol)[3].isdigit():
-                            idx_num = int(str(symbol)[3]) # this method fails to check that the notation for the constant term is closed on the right side with a curly brace
+                            idx_num = int(str(symbol)[3])
+                            # this method fails to check that the notation for the
+                            # constant term is closed on the right side with a curly brace
                         else:
                             print("MTX 1: Your IVP constants don't have proper numerical indices!")
                     else:
@@ -100,12 +105,12 @@ def solution_to_matrix(gen_sol, *args, **kwargs):
             
                 
 def ivp_calculation(matrix,ivp_input=[]):
-    
-    ### IVP Input form being a matrix should be structured with the vector equation below satisfied:
-    ### \vec{f} = A\vec{b}
-    ### where f_{i} = c_{0}*term_{0}(i) + c_{1}*term_{1}(i) + ... + c_{n}*term_{n}(i)
-    ### so that the constants, c_{j}, can be solved using row reduction and returned as a list
-
+    """
+     IVP Input form being a matrix should be structured with the vector equation below satisfied:
+     \vec{f} = A\vec{b}
+     where f_{i} = c_{0}*term_{0}(i) + c_{1}*term_{1}(i) + ... + c_{n}*term_{n}(i)
+     so that the constants, c_{j}, can be solved using row reduction and returned as a list
+    """
     npFlag = False
 
 
@@ -136,7 +141,7 @@ def ivp_calculation(matrix,ivp_input=[]):
         print(f"IVP 5: Original matrix is: {sp_matrix}")
         sp_matrix = sp_matrix.rref()[0]
         print(f"Solved matrix is: {sp_matrix}")
-        print('\n')
+        print("\n")
 
         constants = []
         for idx in range(sp_matrix.rows):
@@ -151,8 +156,7 @@ def ivp_calculation(matrix,ivp_input=[]):
                 
 
         return constants
-
-        
+  
 
 def recursion_solver(input_form):
     sol_eq = sympy.sympify(0)
@@ -189,42 +193,43 @@ def recursion_solver(input_form):
                 continue
             if not root.is_real:
                 imRootFlag = True
-                c_temp = sympy.symbols('c' + str(idx))
-                c_temp2 = sympy.symbols('c' + str(idx+1))
+                c_temp = sympy.symbols("c" + str(idx))
+                c_temp2 = sympy.symbols("c" + str(idx+1))
                 a = sympy.re(root)
                 b = sympy.im(root)
-                sol_eq += c_temp*sympy.cos(n*sympy.acos(a/sympy.sqrt(a**2 + b**2))) + c_temp2*sympy.sin(n*sympy.Abs(sympy.asin(b/sympy.sqrt(a**2 + b**2))))
+                sol_eq += c_temp*sympy.cos(n*sympy.acos(a/sympy.sqrt(a**2 + b**2)))
+                        + c_temp2*sympy.sin(n*sympy.Abs(sympy.asin(b/sympy.sqrt(a**2 + b**2))))
                 idx += 2
             else:
-                c_temp = sympy.symbols('c' + str(idx))
+                c_temp = sympy.symbols("c" + str(idx))
                 sol_eq += c_temp*(root**n)
                 idx += 1
 
-            print('\n')
+            print("\n")
 
     print(sol_eq)
-    print('\n')
+    print("\n")
     return sol_eq
 
 
-
 def recursion_format_processing(input_form, *args, **kwargs):
+    """
+     Vector format for recursion solving should be a row vector
+     with the i-th entry corresponding to the constant multiple of the f_{n-i} term of the recursive sequence equation 
+     The FINAL output will be a symbolic expression corresponding to the general real solution to the homogenous equation
     
-    ### Vector format for recursion solving should be a row vector
-    ### with the i-th entry corresponding to the constant multiple of the f_{n-i} term of the recursive sequence equation 
-    ### The FINAL output will be a symbolic expression corresponding to the general real solution to the homogenous equation
-    ###
-    ### If a specific solution is desired over the general solution,
-    ### the vector format will require two row vectors where the second vector should have 
-    ### specified values corresponding to the value of f_{0}, f_{1}, and so on.
-    ###
-    ### String format should either be text corresponding to latex or sympy code
-    ###
-    ### Sympy format ignores variables not of the form of "f_{n-k}" or "f(n-k)" or "f_n-k"
-    ### where f can be any string of characters that are either first encountered
-    ### or are the 
-    ### Sympy format could also work independently of the choice of symbols for your variables
-    ### and just read left-to-right interpreting the i-th encountered variable as the f_{n-i} term
+     If a specific solution is desired over the general solution,
+     the vector format will require two row vectors where the second vector should have 
+     specified values corresponding to the value of f_{0}, f_{1}, and so on.
+    
+     String format should either be text corresponding to latex or sympy code
+    
+     Sympy format ignores variables not of the form of "f_{n-k}" or "f(n-k)" or "f_n-k"
+     where f can be any string of characters that are either first encountered
+     or are the 
+     Sympy format could also work independently of the choice of symbols for your variables
+     and just read left-to-right interpreting the i-th encountered variable as the f_{n-i} term
+    """
     # Could implement sympy ignore_name flag in parameters for this
     # Can work on seeing if a given set of initial conditions are sufficient to specify an entire solution
     # as this seems plausible under certain structures of the sequence relation
@@ -266,8 +271,10 @@ def recursion_format_processing(input_form, *args, **kwargs):
     return np.array([]),np.array([]), False 
 
 
-
 def sym_recursion_solver_main(sequence, *args, **kwargs):
+    """
+        Insert docstrings here
+    """
     try:
         print("\nWe're doing this main baby!!!")
         print(f"Current sequence is: {sequence}")
